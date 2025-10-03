84f67ff9f7e925fc3c184357b2598d72ec194db2
shirosnaps-code <shirosnaps@gmail.com>
2025-10-04 00:01:45 +0800

    Update server.js

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.post('/generate', async (req, res) => {
 
   try {
     const browser = await puppeteer.launch({
-      headless: true,
-      args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+		headless: true,
+		args: ['--no-sandbox', '--disable-setuid-sandbox'],
+	});
 
     const page = await browser.newPage();
     await page.setContent(html, { waitUntil: 'networkidle0' });
